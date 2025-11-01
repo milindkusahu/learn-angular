6bff4746e83c4b16e1484c445977458b1682d74d
Milind Kumar Sahu <41789140+milindkusahu@users.noreply.github.com>
2025-11-01 07:46:04 +0530

    Components in Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
+import { Signup } from './signup/signup';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Login, Signup],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('learn-angular');
   name = 'milind';
-
 }
