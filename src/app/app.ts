473e5b7f8783ae2a54b7ec3ec3f5eda05a3c22cc
Milind Kumar Sahu <41789140+milindkusahu@users.noreply.github.com>
2025-11-10 18:50:51 +0530

    Events in Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,31 +7,11 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.css',
 })
 export class App {
-  count: number = 0;
+  handleEvent(event: Event) {
+    console.log('Function Called', event.type);
+    console.log('Function Called', event.target);
+    console.log('Function Called', (event.target as Element).className);
 
-  // increment() {
-  //   this.count++;
-  // }
-
-  // decrement() {
-  //   if (this.count > 0) {
-  //     this.count--;
-  //   }
-  // }
-
-  // reset() {
-  //   this.count = 0;
-  // }
-
-  handleCounter(val: string) {
-    if (val === 'decrement') {
-      if (this.count > 0) {
-        this.count--;
-      }
-    } else if (val === 'increment') {
-      this.count++;
-    } else if (val === 'reset') {
-      this.count = 0;
-    }
+    console.log('Value', (event.target as HTMLInputElement).value);
   }
 }
