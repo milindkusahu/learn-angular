61d4678adeeab758ce6e724e9cd8c86b6461893c
Milind Kumar Sahu <41789140+milindkusahu@users.noreply.github.com>
2025-11-11 01:04:22 +0530

    Else If | Contorl Flow

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,9 +7,32 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.css',
 })
 export class App {
-  display = true;
+  color: string = '';
 
-  toggle() {
-    this.display = !this.display;
+  selectColor(val: string) {
+    if (val === 'red') {
+      this.color = 'red';
+    } else if (val === 'green') {
+      this.color = 'green';
+    } else if (val === 'yellow') {
+      this.color = 'yellow';
+    } else if (val === 'other') {
+      this.color = 'blue';
+    } else {
+      this.color = 'white';
+    }
+  }
+
+  handleInput(event: Event) {
+    let val = Number((event.target as HTMLInputElement).value);
+    if (val === 1) {
+      this.color = 'red';
+    } else if (val === 2) {
+      this.color = 'green';
+    } else if (val === 3) {
+      this.color = 'yellow';
+    } else {
+      this.color = 'blue';
+    }
   }
 }
